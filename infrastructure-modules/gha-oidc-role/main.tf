f011f7818c393e2df8a5836564847ad5e3b6130d
keneojiteli <ojitelikenechukwu@gmail.com>
2025-12-06 12:20:42 +0100

    add modules for OIDC auth & role to assume

---
+++ infrastructure-modules/gha-oidc-role/main.tf
@@ -0,0 +1,50 @@
+# tells terraform to expect remote state configuration
+terraform {
+  backend "s3" {}
+}
+
+resource "aws_iam_role" "this" {
+  name               = "gh-actions-${var.environment}-role"
+  assume_role_policy = data.aws_iam_policy_document.trust.json
+}
+
+data "aws_iam_policy_document" "trust" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [var.oidc_provider_arn]   # ensure to use ARN, not URL
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = [
+        "repo:${var.github_repo}:ref:refs/heads/${var.environment}"
+      ]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "combined" {
+  dynamic "statement" {
+    for_each = var.permissions
+    content {
+      sid       = statement.value.sid
+      actions   = statement.value.actions
+      resources = statement.value.resources
+      effect    = "Allow"
+    }
+  }
+}
+
+resource "aws_iam_policy" "this" {
+  name   = "gh-actions-${var.environment}-policy"
+  policy = data.aws_iam_policy_document.combined.json
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.this.arn
+}
